9c3968c712c74e8285a065b9c885354fd5f0e439
bagelbyte <ryanoneal91@yahoo.com>
2018-04-13 20:38:20 -0400

    moved routes to thge auth group, created catch all route to redirect to /

--- routes/web.php
+++ routes/web.php
@@ -11,46 +11,53 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home', 'AppController@index');
-// Route::get('/home', 'AppController@index')->name('main')->middleware('auth');
+// Route::get('/', 'AppController@index');
 
-Route::group(['middleware' => ['permission:create surveys']], function () {
+
+Route::group(['middleware' => ['permission:create surveys', 'auth']], function () {
     Route::post('/survey', 'SurveyController@addNewSurvey');
     Route::get('/surveys/all', 'SurveyController@getAllSurveys');
-    // Route::post('/survey', 'SurveyController@addNewSurvey')->middleware('auth');
 });
 
-Route::get('/isAdmin', function(){
-    $user = \Auth::user();
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/survey/{id}', 'SurveyController@getSurvey');
+    Route::get('/surveys', 'SurveyController@getSurveys');
+    Route::get('/surveys/todo', 'SurveyController@getAllUserAvailableSurveys');
+    Route::post('/survey/{id}', 'SurveyController@takeSurvey');
 
-    return response()->json(['admin' => $user->hasRole('admin')]);
+    Route::get('/isAdmin', function(){
+        $user = \Auth::user();
 
+        return response()->json(['admin' => $user->hasRole('admin')]);
+
+    });
+
+    Route::get('/', 'AppController@index')->name('main');
+
+    //to give yourself admin
+    // Route::get('/gimmeAdmin', function(){
+    //     $user = \Auth::user();
+    //     $user->assignRole('admin');
+    //
+    //     echo 'done genie';
+    // });
 });
+
+Route::get('{any}', function ($any) {
+        return redirect('/');
+})->where('any', '.*');
 
 //survey routes
-Route::get('/survey/{id}', 'SurveyController@getSurvey');
+
 // Route::get('/survey/{id}', 'SurveyController@getSurvey')->middleware('auth');
-Route::get('/surveys', 'SurveyController@getSurveys');
+
 // Route::get('/surveys', 'SurveyController@getSurveys')->middleware('auth');
-Route::get('/surveys/todo', 'SurveyController@getAllUserAvailableSurveys');
+
 // Route::get('/surveys/todo', 'SurveyController@getAllUserAvailableSurveys')->middleware('auth');
-Route::post('/survey/{id}', 'SurveyController@takeSurvey');
+
 // Route::post('/survey/{id}', 'SurveyController@takeSurvey')->middleware('auth');
 
 //only use for initilization!!!
 // Route::get('/init', 'InitController@start');
-
-//to give yourself admin
-// Route::get('/gimmeAdmin', function(){
-//     $user = \Auth::user();
-//     $user->assignRole('admin');
-//
-//     echo 'done genie';
-// });
